a4f8f5c85703ee4cb583e509f0758107f70f9577
Christopher Reece <mrkita.reece@gmail.com>
2019-06-18 16:21:30 +0900

    created an alias for mentor-ui-consttants less

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -2,17 +2,22 @@ const path = require('path');
 
 // Export a function. Accept the base config as the only param.
 module.exports = async ({ config, mode }) => {
-  // `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
-  // You can change the configuration based on that.
-  // 'PRODUCTION' is used when building the static version of storybook.
+	// `mode` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+	// You can change the configuration based on that.
+	// 'PRODUCTION' is used when building the static version of storybook.
 
-  // Make whatever fine-grained changes you need
-  config.module.rules.push({
-    test: /\.less$/,
-    use: ['style-loader', 'css-loader', 'less-loader'],
-    include: path.resolve(__dirname, '../'),
-  });
+	// enable less
+	config.module.rules.push({
+		test: /\.less$/,
+		use: ['style-loader', 'css-loader', 'less-loader'],
+		include: path.resolve(__dirname, '../'),
+	});
 
-  // Return the altered config
-  return config;
+	// resolvers
+	config.resolve.alias['mentor-ui-constants'] = path.resolve(__dirname, '../src/styles/index.less');
+
+
+
+	// Return the altered config
+	return config;
 };
