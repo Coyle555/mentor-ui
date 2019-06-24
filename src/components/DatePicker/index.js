731cc32463b2649285d2c99067aec658d8763d8d
ahstein3521 <adamhs3521@gmail.com>
2019-06-24 18:16:29 -0400

    bundle clean up

--- src/components/DatePicker/index.js
+++ src/components/DatePicker/index.js
@@ -2,12 +2,10 @@ import { Calendar as A } from './components/Calendar';
 import { CalendarDay as B } from './components/CalendarDay';
 import { DatePicker as C } from './components/DatePicker';
 import { DatePickerComposed as D } from './components/DatePickerComposed';
-import { Slider as E } from './components/Slider';
-import { Time as F } from './components/Time';
+import { Time as E } from './components/Time';
 
 export const Calendar = A;
 export const CalendarDay = B;
 export const DatePicker = C;
 export const DatePickerComposed = D;
-export const InputSlider = E;
-export const Time = F;
+export const Time = E;
