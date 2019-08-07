418814b1300d0d05514b19ac11c4c61e2d603893
Christopher Reece <mrkita.reece@gmail.com>
2019-08-08 08:40:05 +0900

    moved datepicker to inputs, verified tests are passing

---
+++ src/components/mentor-inputs/src/components/DatePicker/index.js
@@ -0,0 +1,4 @@
+import { default as Calendar} from './components/Calendar';
+import { default as DatePicker } from './components/DatePicker';
+import { default as DatePickerComposed } from './components/DatePickerComposed';
+import { default as Time } from './components/Time';
