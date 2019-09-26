3e3ef72e53f4e7881a820c8e234966d0ba6d5b00
makkoli <makkoli@vt.edu>
2019-09-26 11:26:54 -0400

    more tests for interacting with hierarchy

--- src/components/Hierarchy/src/__jest__/index.test.js
+++ src/components/Hierarchy/src/__jest__/index.test.js
@@ -73,6 +73,8 @@ const tree = {
 	data: { sample: 'data' }
 };
 
+afterEach(cleanup);
+
 test('Rendering hierarchy that is not virtualized', () => {
 	const json = renderer.create(<Tree isVirtualized={false} tree={tree} />).toJSON();
 
@@ -106,7 +108,7 @@ test('Expanding a node in the tree', () => {
 	expect(queryByText('1-2')).toBeTruthy();
 });
 
-test.only('Expanding a node in the tree w/ an async expand node function', async () => {
+test('Expanding a node in the tree w/ an async expand node function', async () => {
 	const onExpandNode = jest.fn((node) => {
 		return [{
 			expanded: false,
@@ -133,4 +135,87 @@ test.only('Expanding a node in the tree w/ an async expand node function', async
 		expect(queryByText('expand1')).toBeTruthy();
 		expect(queryByText('expand2')).toBeTruthy();
 	});
+});
+
+test('Custom buttons opened menu', async () => {
+	const customButtons = [<button type="button">Example Button</button>];
+
+	const { container, queryByText } = render(
+		<Tree customButtons={customButtons} isVirtualized={false} tree={tree} />
+	);
+
+	fireEvent.click(container.querySelector('button.node-buttons'));
+
+	await wait(() => {
+		expect(queryByText('Example Button')).toBeTruthy();
+	});
+});
+
+test('Selecting a node', () => {
+	const newTree = {
+		id: '1',
+		expanded: false,
+		title: 'Root',
+		children: []
+	};
+
+	const onNodeClick = jest.fn();
+	const { container, queryByText } = render(
+		<Tree isVirtualized={false} onNodeClick={onNodeClick} tree={newTree} />
+	);
+
+	fireEvent.click(queryByText('Root'));
+	expect(container.querySelector('div.mui-node-selected')).toBeTruthy();
+	expect(onNodeClick).toHaveBeenCalledWith({
+		id: '1',
+		expanded: false,
+		title: 'Root',
+		childrenCount: 0,
+		descendants: 0,
+		hasSibling: false,
+		level: 0,
+		parent: null
+	});
+});
+
+test('Selecting and deselecting a node', () => {
+	const onNodeClick = jest.fn();
+	const { container, queryByText } = render(
+		<Tree isVirtualized={false} onNodeClick={onNodeClick} tree={tree} />
+	);
+
+	fireEvent.click(queryByText('1'));
+	fireEvent.click(queryByText('1'));
+	expect(onNodeClick).toHaveBeenCalledTimes(2);
+	expect(onNodeClick).toHaveBeenLastCalledWith(null);
+});
+
+test('Selecting a node and collapsing its ancestor', () => {
+	const { container, queryByText } = render(<Tree isVirtualized={false} tree={tree} />);
+
+	fireEvent.click(queryByText('1-1-1'));
+	fireEvent.click(container.querySelector('button.node-collapse-button'));
+	expect(container.querySelector('div.mui-node-selected')).toBeNull();
+});
+
+test.only('Selecting a node and collapsing another branch after the node in the list', () => {
+	const { container, debug, queryByText } = render(<Tree isVirtualized={false} tree={tree} />);
+
+	fireEvent.click(queryByText('1-2'));
+	// dont want to collapse root
+	fireEvent.click(container.querySelectorAll('button.node-collapse-button')[1]);
+
+	// the third node should still be selected
+	expect(container.querySelector('div.mui-node-selected')).toBeTruthy();
+});
+
+test.skip('Selecting a node and expanding another branch after the node in the list', () => {
+	const { container, debug, queryByText } = render(<Tree isVirtualized={false} tree={tree} />);
+
+	fireEvent.click(queryByText('1-2'));
+	// dont want to collapse root
+	fireEvent.click(container.querySelectorAll('button.node-collapse-button')[1]);
+
+	// the third node should still be selected
+	expect(container.querySelector('div.mui-node-selected')).toBeTruthy();
 });
