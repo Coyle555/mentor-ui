ffb1c5a787d1f1112ac500fb35ac2a43e2f2aeb0
makkoli <makkoli@vt.edu>
2019-10-10 09:50:32 -0400

    setting up drag table example

---
+++ src/components/kyle-tables/src/__stories__/DraggableTable.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import HTML5Backend from 'react-dnd-html5-backend';
+import { useDrop, DndProvider } from 'react-dnd';
+
+import { Table } from '../index';
+
+const DraggableArea = (props) => {
+	const [collectedProps, drop] = useDrop({ accept: 'TABLE_DRAG' });
+
+	return (
+		<div
+			ref={drop}
+			style={{
+				background: 'lightgrey',
+				height: window.innerHeight + 'px',
+				width: '50%',
+				fontSize: '2rem',
+				float: 'left',
+				display: 'flex',
+				justifyContent: 'center',
+				alignItems: 'center'
+			}}
+		>
+			Drop Rows Here
+		</div>
+	);
+};
+
+export const DraggableTable = ({ columns, data }) => {
+
+	return (
+		<DndProvider backend={HTML5Backend}>
+			<DraggableArea />
+			<div style={{
+				height: window.innerHeight + 'px',
+				width: '50%',
+				float: 'right'
+			}}>
+				<Table 
+					columns={columns}
+					currentPage={1}
+					data={data}
+					pageSize={10}
+					recordCount={10}
+				/>
+			</div>
+		</DndProvider>
+	);
+};
+
