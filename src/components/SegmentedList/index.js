0d1f3bf4728e2a230533a6aa4445a12d2ce08dba
makkoli <makkoli@vt.edu>
2019-11-20 09:33:36 -0500

    removed segmented list; publish

--- src/components/SegmentedList/index.js
+++
@@ -1,3 +0,0 @@
-import { SegmentedList } from './components';
-
-export default SegmentedList;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import b from './components/Drawer';
 import c from './components/InsertPopupForm/index';
 import e from './components/Hierarchy';
 import f from './components/ToggleList';
-import g from './components/SegmentedList';
 import { Table as h, TableComponent as i } from './components/kyle-tables/index';
 import { DatePicker as j } from './components/DatePicker';
 
@@ -25,7 +24,6 @@ export const Drawer = b;
 export const InsertPopupForm = c;
 export const Hierarchy = e;
 export const ToggleList = f;
-export const SegmentedList = g;
 export const Table = h;
 export const TableComponent = i;
 export const DatePicker = j;
