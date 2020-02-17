22fa1667789d1fcaa83e444cbd1b14568dca15ff
makkoli <makkoli@vt.edu>
2020-02-17 13:24:20 -0500

    modal class fix

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -47,7 +47,7 @@ export const Modal = props => {
 	if (!props.display) return null;
 
 	const overlayClassName = cn(
-		{ [props.customClasses.overlayClassName]: !!props.customClasses.overlay },
+		{ [props.customClasses.overlay]: !!props.customClasses.overlay },
 		'APM-modal-overlay'
 	);
 
