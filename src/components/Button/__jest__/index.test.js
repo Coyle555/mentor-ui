21a42e8f7bf957daa20981774ae37b2b1800630d
Alex Joyner <alex@voicir.com>
2020-06-02 12:57:46 -0400

    fixed button tests that should be snapshots

--- src/components/Button/__jest__/index.test.js
+++ src/components/Button/__jest__/index.test.js
@@ -5,15 +5,7 @@ import { Button } from '../index';
 
 afterEach(cleanup)
 
-export const TEST_BTN_PREFIX = 'APMButton';
-
 describe('Button Component', () => {
-  test('is an html button', async () => {
-    const { findByRole } = render(<Button>Test Button</Button>);
-    const button = await findByRole('button')
-    expect(button).toBeDefined();
-  })
-
   test('button displays proper text', () => {
     const { getByText } = render(<Button>Test Button</Button>);
     expect(getByText('Test Button')).toBeDefined();
@@ -28,11 +20,6 @@ describe('Button Component', () => {
     expect(testClick.mock.calls.length).toBe(1);
   })
 
-  test('allows misc button options', () => {
-    const { getByText } = render(<Button name="test">Named Button</Button>);
-    expect(getByText('Named Button')).toHaveAttribute('name', 'test')
-  })
-
   test('allows forward refs', async () => {
     const testRef = React.createRef();
     const { findByRole } = render(<Button ref={testRef}>Ref Button</Button>);
@@ -40,13 +27,17 @@ describe('Button Component', () => {
     expect(testRef.current).toContainElement(button);
   })
 
-  test('allows additional classnames', () => {
-    const { getByText } = render(<Button className="test-class">Foo</Button>);
-    expect(getByText('Foo')).toHaveClass(`test-class`)
+  // UI Snapshot tests
+  test('allows misc button options', () => {
+    const tree = renderer.create(<Button name="bar">Foo</Button>).toJSON();
+    expect(tree).toMatchSnapshot();
   })
 
+  test('allows additional classnames', () => {
+    const tree = renderer.create(<Button className="bar">Foo</Button>).toJSON();
+    expect(tree).toMatchSnapshot();
+  })
 
-  // UI Snapshot tests
   test('default button case', () => {
     const tree = renderer.create(<Button>Foo</Button>).toJSON();
     expect(tree).toMatchSnapshot();
