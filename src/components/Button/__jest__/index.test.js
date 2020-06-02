31cf9701a0f1d8c389fd41a6717940ca6e939123
Alex Joyner <alex@voicir.com>
2020-06-02 14:20:59 -0400

    removed unnecessary button tests

--- src/components/Button/__jest__/index.test.js
+++ src/components/Button/__jest__/index.test.js
@@ -1,25 +1,11 @@
 import React from 'react';
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 import { Button } from '../index';
 
 afterEach(cleanup)
 
 describe('Button Component', () => {
-  test('button displays proper text', () => {
-    const { getByText } = render(<Button>Test Button</Button>);
-    expect(getByText('Test Button')).toBeDefined();
-  })
-
-  test('button allows being clicked', () => {
-    const testClick = jest.fn();
-    const { getByText } = render(
-      <Button onClick={testClick}>Test Click</Button>
-    );
-    fireEvent.click(getByText('Test Click'));
-    expect(testClick.mock.calls.length).toBe(1);
-  })
-
   test('allows forward refs', async () => {
     const testRef = React.createRef();
     const { findByRole } = render(<Button ref={testRef}>Ref Button</Button>);
