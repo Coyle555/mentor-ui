6aba59467127c46c9a80d48faa2f4dc030c4a7e5
Alex Joyner <alex@voicir.com>
2020-06-03 11:24:26 -0400

    fixed oneOfType aregument warning

--- src/components/mentor-inputs/src/components/float-input/floatInput.js
+++ src/components/mentor-inputs/src/components/float-input/floatInput.js
@@ -5,58 +5,58 @@ import classNames from 'classnames';
 import TextInput from '../text-input/textInput'
 
 function isFloat(num) {
-	return !isNaN(num) && parseFloat(Number(num)) === Number(num) && !isNaN(parseFloat(num, 10))
-		? true
-		: 'Invalid number';
+  return !isNaN(num) && parseFloat(Number(num)) === Number(num) && !isNaN(parseFloat(num, 10))
+    ? true
+    : 'Invalid number';
 }
 
 const FloatInput = ({ max, min, precision, validate, ...props }) => {
 
-	const hasValidPrecision = useCallback(val => (
-		precision > -1 && String(Number(val).toFixed(precision)) !== val
-			? 'Invalid number'
-			: true
-	), [precision]);
+  const hasValidPrecision = useCallback(val => (
+    precision > -1 && String(Number(val).toFixed(precision)) !== val
+      ? 'Invalid number'
+      : true
+  ), [precision]);
 
-	const isGreaterThanMin = useCallback(value => (
-		typeof min === 'number' && Number(value) < min
-			? 'Value is too small'
-			: true
-	), [min]);
+  const isGreaterThanMin = useCallback(value => (
+    typeof min === 'number' && Number(value) < min
+      ? 'Value is too small'
+      : true
+  ), [min]);
 
-	const isGreaterThanMax = useCallback(value => (
-		typeof max === 'number' && Number(value) > max
-			? 'Value is too large'
-			: true
-	), [max]);
+  const isGreaterThanMax = useCallback(value => (
+    typeof max === 'number' && Number(value) > max
+      ? 'Value is too large'
+      : true
+  ), [max]);
 
-	const validates = useMemo(() => {
-		return [
-			isFloat,
-			hasValidPrecision,
-			isGreaterThanMin,
-			isGreaterThanMax
-		].concat(validate);
-	}, [hasValidPrecision, isGreaterThanMin, isGreaterThanMax, validate]);
+  const validates = useMemo(() => {
+    return [
+      isFloat,
+      hasValidPrecision,
+      isGreaterThanMin,
+      isGreaterThanMax
+    ].concat(validate);
+  }, [hasValidPrecision, isGreaterThanMin, isGreaterThanMax, validate]);
 
-	return (
-		<TextInput
-			placeholder="Enter decimal"
-			{...props}
-			validate={validates}
-		/>
-	);
+  return (
+    <TextInput
+      placeholder="Enter decimal"
+      {...props}
+      validate={validates}
+    />
+  );
 };
 
 FloatInput.propTypes = {
-	max: PropTypes.number,
-	min: PropTypes.number,
-	precision: PropTypes.number,
-	validate: PropTypes.oneOfType(PropTypes.array, PropTypes.func)
+  max: PropTypes.number,
+  min: PropTypes.number,
+  precision: PropTypes.number,
+  validate: PropTypes.oneOfType([PropTypes.array, PropTypes.func])
 };
 
 FloatInput.defaultProps = {
-	precision: -1
+  precision: -1
 };
 
 export default FloatInput;
