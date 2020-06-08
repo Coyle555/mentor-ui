842ddb7f326e9222d5ae79bd75fa122f22419a75
Alex Joyner <alex@voicir.com>
2020-06-08 10:41:20 -0400

    updated tests for select-input

--- src/components/mentor-inputs/src/components/select-input/selectInput.js
+++ src/components/mentor-inputs/src/components/select-input/selectInput.js
@@ -5,183 +5,182 @@ import classNames from 'classnames';
 import '../../styles/index.less';
 
 function hasError(value, required, customValidators) {
-	if (value === '' && !!required) {
-		return 'Value is required';
-	}
+  if (value === '' && !!required) {
+    return 'Value is required';
+  }
 
-	if (!!customValidators && !Array.isArray(customValidators)) {
-		customValidators = [customValidators];
-	}
+  if (!!customValidators && !Array.isArray(customValidators)) {
+    customValidators = [customValidators];
+  }
 
-	if (value !== ''
-		&& !!customValidators
-		&& customValidators.length > 0) {
+  if (value !== ''
+    && !!customValidators
+    && customValidators.length > 0) {
 
-		for (let validator of customValidators) {
-			if (typeof validator === 'function') {
-				const validity = validator(value);
-				
-				if (typeof validity === 'string') {
-					return validity;
-				} else if (!validator(value)) {
-					return 'An error occurred';
-				}
-			}
-		}
-	}
+    for (let validator of customValidators) {
+      if (typeof validator === 'function') {
+        const validity = validator(value);
 
-	return false;
+        if (typeof validity === 'string') {
+          return validity;
+        } else if (!validator(value)) {
+          return 'An error occurred';
+        }
+      }
+    }
+  }
+
+  return false;
 }
 
-const SelectInput = ({ 
-	disabled,
-	options, 
-	placeholder,
-	parse, 
-	parseMatchedValue,
-	required,
-	validate, 
-	...props 
+const SelectInput = ({
+  disabled,
+  options,
+  placeholder,
+  parse,
+  parseMatchedValue,
+  required,
+  validate,
+  ...props
 }) => {
-	let value = typeof parseMatchedValue === 'function'
-		? parseMatchedValue(props.value)
-		: props.value;
+  let value = typeof parse === 'function'
+    ? parse(props.value)
+    : props.value;
 
-	if (value === undefined || value === null) {
-		value = '';
-	}
+  if (value === undefined || value === null) {
+    value = '';
+  }
 
-	const inputRef = useRef(null);
-	const [ currentValue, setCurrentValue ] = useState(value);
-	const [ error, setError ] = useState(() => hasError(value, required, validate));
+  const inputRef = useRef(null);
+  const [currentValue, setCurrentValue] = useState(value);
+  const [error, setError] = useState(() => hasError(value, required, validate));
 
-	useEffect(() => {
-		setError(hasError(currentValue, required, validate));
-	}, [required, validate]);
+  useEffect(() => {
+    setError(hasError(currentValue, required, validate));
+  }, [required, validate]);
 
-	useEffect(() => {
-		let value = typeof parseMatchedValue === 'function'
-			? parseMatchedValue(props.value)
-			: props.value;
+  useEffect(() => {
+    let value = typeof parse === 'function'
+      ? parse(props.value)
+      : props.value;
+    if (value === undefined || value === null) {
+      value = '';
+    }
 
-		if (value === undefined || value === null) {
-			value = '';
-		}
+    if (currentValue !== value) {
+      setCurrentValue(value);
+      setError(hasError(value, required, validate));
+    }
+  }, [props.value]);
 
-		if (currentValue !== value) {
-			setCurrentValue(value);
-			setError(hasError(value, required, validate));
-		}
-	}, [props.value]);
+  /// as soon as the input ref is attached to the node
+  /// check for errors on the value
+  /// (We also need to reevaluate error status if required attribute is changed)
+  useEffect(() => {
+    if (!inputRef.current) return;
 
-	/// as soon as the input ref is attached to the node
-	/// check for errors on the value
-	/// (We also need to reevaluate error status if required attribute is changed)
-	useEffect(() => {
-		if (!inputRef.current) return;
+    const err = hasError(currentValue, required, validate);
+    setError(err);
 
-		const err = hasError(currentValue, required, validate);
-		setError(err);
+    if (typeof err === 'string') {
+      inputRef.current.setCustomValidity(err);
+    }
+  }, [inputRef.current, currentValue, required, validate]);
 
-		if (typeof err === 'string') {
-			inputRef.current.setCustomValidity(err);
-		}
-	}, [inputRef.current, currentValue, required, validate]);
+  const onBlur = evt => {
+    if (typeof props.onBlur !== 'function') return;
 
-	const onBlur = evt => {
-		if (typeof props.onBlur !== 'function') return;
-		
-		const actualValue = typeof parse === 'function'
-			&& typeof parseMatchedValue !== 'function'
-				? options.find(opt => parse(opt) === currentValue)
-				: currentValue;
+    const actualValue = typeof parse === 'function'
+      && typeof parseMatchedValue !== 'function'
+      ? options.find(opt => parse(opt) === currentValue)
+      : currentValue;
 
-		props.onBlur(error, actualValue, props.name, evt);
-	};
+    props.onBlur(error, actualValue, props.name, evt);
+  };
 
-	const onChange = useCallback(evt => {
-		const newValue = evt.target.value;
-		setCurrentValue(newValue);
+  const onChange = useCallback(evt => {
+    const newValue = evt.target.value;
+    setCurrentValue(newValue);
 
-		const error = hasError(newValue, required, validate);
-		setError(error);
+    const error = hasError(newValue, required, validate);
+    setError(error);
 
-		if (typeof props.onChange === 'function') {
-			
-			const actualValue = typeof parse === 'function'
-				&& typeof parseMatchedValue !== 'function'
-					? options.find(opt => parse(opt) === newValue)
-					: newValue;
+    if (typeof props.onChange === 'function') {
 
-			if (inputRef.current) {
-				if (error) {
-					inputRef.current.setCustomValidity(String(error));
-				} else {
-					inputRef.current.setCustomValidity('');
-				}
-			}
+      const actualValue = typeof parse === 'function'
+        && typeof parseMatchedValue !== 'function'
+        ? options.find(opt => parse(opt) === newValue)
+        : newValue;
 
-			props.onChange(error, actualValue, props.name, evt);
-		}
-	}, [inputRef.current, options, parse, parseMatchedValue, props.name, props.onChange, required, validate]);
+      if (inputRef.current) {
+        if (error) {
+          inputRef.current.setCustomValidity(String(error));
+        } else {
+          inputRef.current.setCustomValidity('');
+        }
+      }
 
-	const inputClasses = classNames({
-		'mui-mi-input-field': true,
-		[props.className]: !!props.className,
-		'mui-mi-input-field-has-error': !!error && !disabled
-	});
+      props.onChange(error, actualValue, props.name, evt);
+    }
+  }, [inputRef.current, options, parse, parseMatchedValue, props.name, props.onChange, required, validate]);
 
-	return (
-		<select
-			{...props}
-			disabled={disabled}
-			required={required}
-			className={inputClasses}
-			name={props.name}
-			onBlur={onBlur}
-			onChange={onChange}
-			ref={inputRef}
-			value={currentValue}
-		>
-			<option
-				disabled={required}
-				value=""
-			>
-				{ placeholder }
-			</option>
-			{ options.map(option => {
+  const inputClasses = classNames({
+    'mui-mi-input-field': true,
+    [props.className]: !!props.className,
+    'mui-mi-input-field-has-error': !!error && !disabled
+  });
 
-				const value = typeof parse === 'function'
-					? parse(option)
-					: option;
+  return (
+    <select
+      {...props}
+      disabled={disabled}
+      required={required}
+      className={inputClasses}
+      name={props.name}
+      onBlur={onBlur}
+      onChange={onChange}
+      ref={inputRef}
+      value={currentValue}
+    >
+      <option
+        disabled={required}
+        value=""
+      >
+        {placeholder}
+      </option>
+      {options.map(option => {
 
-				const returnValue = typeof parseMatchedValue === 'function' 
-					? parseMatchedValue(option)
-					: value;
+        const value = typeof parse === 'function'
+          ? parse(option)
+          : option;
 
-				return (
-					<option
-						key={value}
-						value={returnValue}
-					>
-						{value}
-					</option>
-				);
-			})}
-		</select>
-	);
+        const returnValue = typeof parseMatchedValue === 'function'
+          ? parseMatchedValue(option)
+          : value;
+
+        return (
+          <option
+            key={value}
+            value={returnValue}
+          >
+            {value}
+          </option>
+        );
+      })}
+    </select>
+  );
 }
 
 SelectInput.propTypes = {
-	options: PropTypes.array,
-	parse: PropTypes.func,
-	validation: PropTypes.func,
+  options: PropTypes.array,
+  parse: PropTypes.func,
+  validation: PropTypes.func,
 };
 
 SelectInput.defaultProps = {
-	options: [],
-	placeholder: 'Select a value',
-	value: ''
+  options: [],
+  placeholder: 'Select a value',
+  value: ''
 };
 
 export default SelectInput;
